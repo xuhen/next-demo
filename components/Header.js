3e9a7c58d6a12b0a533aa056d8a2d2a748ba70c6
edward.xu <edward.xu@qunar.com>
2019-09-25 16:50:55 +0800

    feat: first commit

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import "./Header.scss";
+
+const Header = (props) => (
+    <div className="Header">
+      {props.appTitle}
+    </div>
+  );
+
+  export default Header;
---
+++ components/NavButton.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { withRouter } from "next/router";
+import "./NavButton.scss";
+
+const NavButton = props => {
+    console.log('props', props);
+    return (
+        <Link href={props.path}>
+            <div
+                className={`NavButton ${
+                    props.router.pathname === props.path ? "active" : ""
+                }`}
+            >
+            <div className="Icon">{props.icon}</div>
+            <span className="Label">{props.label}</span>
+            </div>
+        </Link>
+    );
+}
+
+export default withRouter(NavButton);
